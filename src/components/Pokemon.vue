72d428da0748635ee2246a852400c7c5603ae8ba
Moritz Kretschmer <moritz.kretschmer@Platz62.ai.local>
2021-09-01 17:03:25 +0200

    pokedex und pokemon comp. typen farben

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,137 @@
+<template>
+    <div class="pokemon p-6" :class="type" >
+        <h1 class="text-center">{{ species }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        species: String,
+        type: String
+    },
+
+    setup() {
+        
+    },
+}
+</script>
+
+<style scoped>
+
+.pokemon {
+    --font-light: #F3F4F6;
+    --font-dark: #1F2937;
+
+    --normal: #A8A77A;
+    --fire: #EE8130;
+    --water: #6390F0;
+    --electric: #F7D02C;
+    --grass: #7AC74C;
+    --ice: #96D9D6;
+    --fighting: #C22E28;
+    --poison: #A33EA1;
+    --ground: #E2BF65;
+    --flying: #A98FF3;
+    --psychic: #F95587;
+    --bug: #A6B91A;
+    --rock: #B6A136;
+    --ghost: #735797;
+    --dragon: #6F35FC;
+    --dark: #705746;
+    --steel: #B7B7CE;
+    --fairy: #D685AD;
+
+}
+
+.normal {
+    background-color: var(--normal);
+    color: var(--font-dark);
+}
+
+.fire {
+    background-color: var(--fire);
+    color: var(--font-dark);
+}
+
+.water {
+    background-color: var(--water);
+    color: var(--font-dark);
+}
+
+.electric {
+    background-color: var(--electric);
+    color: var(--font-dark);
+}
+
+.grass {
+    background-color: var(--grass);
+    color: var(--font-dark);
+}
+
+.ice {
+    background-color: var(--ice);
+    color: var(--font-dark);
+}
+
+.fighting {
+    background-color: var(--fighting);
+    color: var(--font-light);
+}
+
+.poison {
+    background-color: var(--poison);
+    color: var(--font-light);
+}
+
+.ground {
+    background-color: var(--ground);
+    color: var(--font-dark);
+}
+
+.flying {
+    background-color: var(--flying);
+    color: var(--font-dark);
+}
+
+.psychic {
+    background-color: var(--psychic);
+    color: var(--font-dark);
+}
+
+.bug {
+    background-color: var(--bug);
+    color: var(--font-dark);
+}
+
+.rock {
+    background-color: var(--rock);
+    color: var(--font-light);
+}
+
+.ghost {
+    background-color: var(--ghost);
+    color: var(--font-light);
+}
+
+.dragon {
+    background-color: var(--dragon);
+    color: var(--font-light);
+}
+
+.dark {
+    background-color: var(--dark);
+    color: var(--font-light);
+}
+
+.steel {
+    background-color: var(--steel);
+    color: var(--font-dark);
+}
+
+.fairy {
+    background-color: var(--fairy);
+    color: var(--font-dark);
+}
+
+</style>
